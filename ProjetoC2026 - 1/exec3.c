4433431942e407940cfdda1b6fc12248e8b37512
jg211 <jbbarrosantos@gmail.com>
2026-05-20 20:40:44 -0300

    Update exec3.c

--- ProjetoC2026 - 1/exec3.c
+++ ProjetoC2026 - 1/exec3.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main() {
+    float temperaturas[7];
+    int i;
+
+    int seguras = 0;
+    int foraFaixa = 0;
+
+    float soma = 0;
+    float media;
+    for(i = 0; i < 7; i++){
+        printf("Digite a temperatura: ");
+        scanf("%f", temperaturas[i]);
+        
+        
+    }
+
+    // Implemente aqui:
+    // 1. Ler as 7 temperaturas e armazenar no vetor temperaturas
+    // 2. Usar if com AND para verificar a faixa segura
+    // 3. Contar temperaturas seguras e fora da faixa
+    // 4. Calcular a média
+
+    return 0;
+}
