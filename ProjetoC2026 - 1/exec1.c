26a6f088cd758709aa606feee78878dde14be2bf
jg211 <jbbarrosantos@gmail.com>
2026-05-20 19:46:03 -0300

    Update exec1.c

--- ProjetoC2026 - 1/exec1.c
+++ ProjetoC2026 - 1/exec1.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+int main() {
+    float notas[8];
+    float frequencia;
+    int i;
+
+    int aprovados = 0;
+    int reprovados = 0;
+
+    float maiorNota = 0;
+    for(i = 0; i < 8; i++){
+        printf("Digite a nota: ");
+        scanf("%f", &notas[i]);
+        printf("Digite a frequencia: ");
+        scanf("%f", &frequencia);
+   if(notas[i] >= 7 && frequencia >= 75){
+       printf("Aprovado!\n");
+       aprovados++;
+   }else{
+       printf("Reprovado!\n");
+       reprovados++;
+   }
+    }
+     maiorNota = notas[i];
+    for(i = 0; i < 8; i++){
+    if(notas[i] > maiorNota){
+           maiorNota = notas[i];
+           }
+    }
+   printf("Total de aprovados:%i\n",aprovados);
+   printf("Total de reprovados:%i\n",reprovados);
+   printf("Maior nota:%.2f", maiorNota);
+    return 0;
+}
