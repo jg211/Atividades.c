d70ab2570fe270c7d028621628ca04775752d899
jg211 <jbbarrosantos@gmail.com>
2026-05-27 18:52:51 -0300

    Update exec1.c

--- Trabalho_Pratico_Introducao_a_Programacao_em_C/exec1.c
+++ Trabalho_Pratico_Introducao_a_Programacao_em_C/exec1.c
@@ -1,3 +1,61 @@
+//PARTE TEORICA:
+//A) O que é pensamento computacional? 
+//Forma de pensar usada para resolver problemas de maneira lógica, organizada em passo a passo. 
+
+//B) Explique os conceitos: 
+
+//Decomposição:  
+//É o processo de dividir um problema grande em partes menores para facilitar a resolução. 
+
+//Abstração: 
+//É simplificar um problema, prestando atenção apenas no que realmente importa para resolver a situação. 
+
+//Reconhecimento de padrões: 
+//É identificar situações semelhantes que se repetem para usar a mesma solução. 
+
+//Algoritmo:  
+//É uma sequência organizada de passos para resolver um problema. 
+
+//C)Explique a diferença entre: 
+
+//AND (&&): 
+//Operador usado quando queremos que duas ou mais condições aconteçam ao mesmo tempo. Onde a condição inteira só será considerada verdadeira se todas as partes forem verdadeiras. 
+
+//OR (||): 
+//Operador que também é usado quando queremos que duas ou mais condições aconteçam ao mesmo tempo. Porém a condição inteira será considerada verdadeira se ao menos uma das partes forem verdadeiras. 
+
+//PARTE 2 — Tabela Verdade 
+
+//Questão 2 — Tabela Verdade 
+
+//Considere: 
+
+//p = idade >= 18 
+//q = possuiCarteira == 1 
+
+ //A) Complete a tabela verdade para: 
+
+//p && q 
+
+//p | q|p && q |
+//V | V | TRUE | 
+//V | F | FALSE |
+//F | V | FALSE |
+//F | F | FALSE |
+
+//B) Complete a tabela verdade para: 
+
+p || q 
+
+//| p | q | p || q | 
+//|---|---|--------| 
+//| V | V |  True   | 
+//| V | F |  True   |      
+//| F | V |  True   | 
+//| F | F | False   | 
+ 
+//PARTE PRÁTICA
+ 
 #include <stdio.h> 
  
 int main() { 
